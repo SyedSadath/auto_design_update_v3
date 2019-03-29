975beb28b8129d8b8d230f29c2daf243cc290326
root <syedsadath101@gmail.com>
2019-03-29 17:24:40 +0530

    first commit

---
+++ jquery.js
@@ -0,0 +1,30 @@
+
+$(document).ready(function(){
+
+
+	$('.banner34').hover(function(){
+
+		$(this).attr('src','img/car2.png');
+
+	});
+
+	$('#particles-js').hover(function(){
+
+		$('.banner34').attr('src','img/car1.png');
+	});
+
+
+
+	// $('.banner34').hover(function(){
+	// 	$(this).attr('src','img/mustang_car.png');
+	// 	$('.mimg').attr('src', 'img/wheel_newd.png');
+	// });
+
+	// $('.mimg').hover(function(){
+	// 	$(this).attr('src', 'img/wheel_new.png');
+	// 	$('.banner34').attr('src','img/mustang_car.png');
+		
+	// });
+		
+});
+
